0f0482f7d82d71c7f641f360cc0ba79d1aafb966
kasiss <kasiss@bogon.hna.net>
2017-08-17 13:14:27 +0800

    controller 增加功能

--- system/core/KsfController.php
+++ system/core/KsfController.php
@@ -12,6 +12,8 @@ class KsfController
 
     public $router = null;
 
+    private $reponse_headers = array();
+
     private $Ksf_instance;
 
     public function __construct()
@@ -35,5 +37,45 @@ class KsfController
     public function getError()
     {
         return $this->Ksf_instance->error;
+    }
+
+    public function getRequest()
+    {
+        return $this->Ksf_instance->input;
+    }
+    
+    public function responseJson($error, $msg, $data = array(), $code = 0, $status = 200)
+    {
+        $this->setHeaders('Content-Type', 'application/json')
+             ->setHeaders('Status', $status);
+        $json = array(
+            'err' => $error,
+            'msg' => $msg,
+            'data' => $data,
+            'code' => $code
+        );
+        $this->doResponse(json_encode($json));
+    }
+    
+    public function responseRaw($content,$status)
+    {
+        $this->setHeaders('Status', $status);
+        $msg = is_array($content) ? json_encode($content) : $content;
+        $this->doResponse($msg);
+    }
+    
+    protected function setHeaders($key,$value)
+    {
+        $this->reponse_headers = array_merge($this->reponse_headers,array($key=>$value));
+        return $this;
+    }
+    private  function doResponse($data)
+    {
+        foreach($this->reponse_headers as $key=>$value)
+        {
+            header($key.': '.$value);
+        }
+        echo $data;
+        die();
     }
 }
